74c585b5c776f874fd92c27896ec9e653c775936
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-05-01 14:07:37 +0600

    feat: Make Hero section responsive
    Adjusted layout, text sizes, image positioning, and card placement within the Hero component to improve adaptability across different screen sizes (mobile, tablet, desktop)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,12 @@ import { Manrope } from "next/font/google";
 import "./globals.css";
 
 const manrope = Manrope({
-  variable: "--font-manrope",
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  style: ["normal"],
   subsets: ["latin"],
+  display: "swap",
+  fallback: ["system-ui", "sans-serif"],
+  variable: "--font-manrope",
 });
 
 export const metadata: Metadata = {
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Header from "@/components/shared/Header";
+import Hero from "@/components/shared/Hero";
 import MobileNav from "@/components/shared/MobileNav";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <header className="fixed top-0 left-0 z-10 flex w-full items-center justify-between bg-gradient-to-tl from-blue-50 from-0% via-yellow-50 via-50% to-blue-200 to-100% px-4 py-4 backdrop-blur-lg">
-        <div className="container mx-auto max-w-6xl">
+    <main>
+      <header className="flex w-full items-center justify-between bg-gradient-to-tl from-white from-40% via-[#004FCE]/10 via-70% to-[#004FCE]/20 to-100% px-4 py-4 backdrop-blur-lg">
+        <div className="container mx-auto max-w-7xl">
           <nav className="flex items-center justify-between">
             <div className="flex items-center space-x-8">
               <div className="flex items-center space-x-1">
@@ -18,13 +19,15 @@ export default function Home() {
             </div>
 
             <Button
-              className="border-accent-foreground ml-auto rounded-full max-md:hidden"
+              className="border-accent-foreground ml-auto rounded-full text-base font-bold max-md:hidden"
               variant="outline"
             >
               Get Started
             </Button>
             <MobileNav />
           </nav>
+
+          <Hero />
         </div>
       </header>
     </main>
